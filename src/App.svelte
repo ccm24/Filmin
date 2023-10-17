0da383f27e7e51032436de16c602fcfd1a81c46f
ccm24 <ccmm0024@gmail.com>
2023-10-17 14:12:12 +0200

    menu

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Menu from './Components/Menu.svelte';
+</script>
+
+
+  <nav class="menu">
+    <Menu />
+  </nav>
+
+
+<style>
+
+</style>
---
+++ src/Components/Menu.svelte
@@ -0,0 +1,35 @@
+<script>
+  import filminLogo from '/Users/corac/Documents/factoriaf5/p11frontend/projects/p11-project4/src/assets/Logo.svg'
+</script>
+
+<main>
+    <img src={filminLogo} class="logo" alt="Logo de Filmin" />
+  <nav>
+    <a href="/">Inicio</a>
+    <a href="/">Películas</a>
+    <a href="/">Series</a>
+    <a href="/">Colecciones</a>
+    <a href="/">Festivales</a>
+    <a href="/">Más</a>
+  </nav>
+</main>
+
+<style>
+    main{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin-top: 33px;
+    }
+    nav{
+        margin-left: 40px;
+    }
+    a{
+        margin-right: 40px;
+        text-decoration: none;
+        color: white;
+        size: 20px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: bold;
+    }
+</style>
